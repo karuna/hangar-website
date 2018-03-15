1154471ee2bf2a0d7088fc70ce10267d5bf3f796
Karuna Murti <karuna.murti@gmail.com>
2018-03-15 23:18:20 +0900

    update layout

--- client/assets/application.js
+++ client/assets/application.js
@@ -1,2 +1,10 @@
-// Start from here. If you run `npm run start-js` it will precompile this
-// automatically to Hangar's assets folder.
+// The following code is based off a toggle menu by @Bradcomp
+// source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
+window.onload = function() {
+    var burger = document.querySelector('.burger');
+    var menu = document.querySelector('#'+burger.dataset.target);
+    burger.addEventListener('click', function() {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    });
+};
